613cfd226ef1d1b53fd66811bbf59846555f03a5
Zakhar Bessarab <me@zekker.dev>
2025-03-03 14:56:48 +0400

    .taskfiles: do not update appVersion as it is done by renovate

--- .taskfiles/merge-renovate-prs.sh
+++ .taskfiles/merge-renovate-prs.sh
@@ -32,13 +32,8 @@ for ref in $refs; do
   # Update chart version, app version and artifacthub.io/changes
   # get commit message
   commit_message=$(git log --format=%B -n 1 origin/$ref | awk -F ': ' '{print $2}')
-  app_version=$(echo $commit_message | awk -F ' to ' '{print $2}' | sed 's/v//')
 
   chart_file=$(echo $files_diff | sed 's/values.yaml/Chart.yaml/')
-
-  if [[ $commit_message != *"redis"* && $commit_message != *"postgres"* ]]; then
-     yq -i '.appVersion = "v'$app_version'"' $chart_file
-  fi
 
   changes=$(cat << EOF
 - kind: changed
